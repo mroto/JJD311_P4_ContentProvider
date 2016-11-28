d9aef6dbc0eaa3b7f7e581645af74c4eaaa6010f
mroto <marc.rodriguez.torrecillas@gmail.com>
2016-11-28 10:41:03 +0100

    ParamsDB constants

--- app/src/main/java/com/mroto/jjd311_p4_contentprovider/data/ParamsDb.java
+++ app/src/main/java/com/mroto/jjd311_p4_contentprovider/data/ParamsDb.java
@@ -4,5 +4,11 @@ package com.mroto.jjd311_p4_contentprovider.data;
  * Created by A5Alumno on 28/11/2016.
  */
 
-public class ParamsDb {
+public abstract class ParamsDb {
+    public static final String DB_NAME="JJD311-Database";
+    public static final String TABLE_NAME="JJD311-Table";
+    public static final int DB_VERSION=1;
+    public static final String _ID="_id";
+    public static final String STUDENT_NAME="name";
+    public static final String STUDENT_AGE="age";
 }
