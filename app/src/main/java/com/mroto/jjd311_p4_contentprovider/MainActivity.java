5918a07cc0c6f8a773d2ad7cd6079409b9824624
mroto <marc.rodriguez.torrecillas@gmail.com>
2016-11-28 10:15:05 +0100

    Initial commit

---
+++ app/src/main/java/com/mroto/jjd311_p4_contentprovider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mroto.jjd311_p4_contentprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
