b76716e1f37f313451f81811808eef811d0fb83a
mroto <marc.rodriguez.torrecillas@gmail.com>
2016-11-28 10:31:16 +0100

    buttons implementations

--- app/src/main/java/com/mroto/jjd311_p4_contentprovider/MainActivity.java
+++ app/src/main/java/com/mroto/jjd311_p4_contentprovider/MainActivity.java
@@ -2,12 +2,40 @@ package com.mroto.jjd311_p4_contentprovider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final static String TAG=MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Click Listeners:
+        findViewById(R.id.btn_query_add).setOnClickListener(this);
+        findViewById(R.id.btn_query_consult).setOnClickListener(this);
+        findViewById(R.id.btn_query_delete).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btn_query_add) {
+            this.dbAdd();
+        }else if(view.getId()==R.id.btn_query_delete) {
+            this.dbDelete();
+        }else if(view.getId()==R.id.btn_query_consult) {
+            this.dbConsult();
+        }
+    }
+
+    private void dbAdd(){
+        //TODO
+    }
+    private void dbDelete(){
+        //TODO
+    }
+    private void dbConsult(){
+        //TODO
     }
 }
